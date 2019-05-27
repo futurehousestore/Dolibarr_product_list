e6d2630698c11213a574debce14cd9cff444fdc5
futurehousestore <futurehousestore@yahoo.com>
2019-05-27 22:14:02 +0100

    fix product label position

--- htdocs/product/list.php
+++ htdocs/product/list.php
@@ -850,7 +850,7 @@ if ($resql)
 		// Label
 		if (! empty($arrayfields['p.label']['checked']))
 		{
-			print '<td class="tdoverflowmax200">'.dol_trunc($obj->label, 80).'</td>';
+			print '<td class="left">'.dol_trunc($obj->label, 80).'</td>';
 			if (! $i) $totalarray['nbfield']++;
 		}
 
